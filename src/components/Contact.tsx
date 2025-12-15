b22ebe1bd20f9dcf540ec015c56df142ff54777c
Jonathan Ostertag <jonathan@ostertage.de>
2025-12-15 13:22:49 +0100

    fix: cv url

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -27,7 +27,7 @@ export const Contact = () => {
     {
       icon: FileText,
       label: t("contact.downloadCV"),
-      href: `/2025-JonathanOstertag-CV-en.pdf`,
+      href: `${import.meta.env.BASE_URL}2025-JonathanOstertag-CV-en.pdf`,
       color: "text-accent",
     },
   ];
